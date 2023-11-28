6eafdaef0ce4396345a12512e627b8b790e9c4f4
oDzzy <samhassanrocks@yahoo.com>
2023-11-27 23:54:38 -0600

    rate limiting

--- src/main/java/com/valorant/skinelo/controllers/SkinController.java
+++ src/main/java/com/valorant/skinelo/controllers/SkinController.java
@@ -3,34 +3,63 @@ package com.valorant.skinelo.controllers;
 
 import com.valorant.skinelo.entity.Skin;
 import com.valorant.skinelo.service.SkinService;
-import org.springframework.http.HttpHeaders;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import java.time.Duration;
 import java.util.List;
-import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/skin")
 public class SkinController {
     private SkinService SS;
+    private final Bucket bucket;
 
     public SkinController(SkinService SS) {
         this.SS = SS;
+        Bandwidth limit = Bandwidth.classic(200, Refill.greedy(200, Duration.ofMinutes(1)));
+        this.bucket = Bucket4j.builder()
+                .addLimit(limit)
+                .build();
     }
 
     @PostMapping
     public Skin createSkin(@RequestBody Skin skin){return SS.createSkin(skin);}
 
     @GetMapping
-    public Skin  getRandomSkin(){return SS.getRandomSkin();}
+    public ResponseEntity<Skin>  getRandomSkin() {
+
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(SS.getRandomSkin());
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+
+    }
+
+
+
+
     @GetMapping("/Leaderboard")
-    public List<Skin> Leaderboard(){return SS.getOrderedSkin();}
+    public ResponseEntity<List<Skin>> Leaderboard(){
+
+        if (bucket.tryConsume(1)) {
+            return ResponseEntity.ok(SS.getOrderedSkin());
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+
+    }
 
     @PostMapping("/UpdateElo")
-    public void updateElo(@RequestBody List<Long> values){SS.setElo(values);}
+    public void updateElo(@RequestBody List<Long> values){
+        if (bucket.tryConsume(1)) {
+            SS.setElo(values);
+        }
+    }
 
 
 }
